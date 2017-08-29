e446f845a9c3ef58c8f9a5d916713bede3554ed7
teddylockhart <teddy.lockhart@gmail.com>
2017-08-28 22:50:33 -0700

    5th JS: Github Hotfix

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,7 +4,7 @@ var topics = ["XCOM 2", "Super Mario", "Sonic Mania", "Persona 5", "Street Fight
 function displayGameGif() {
 
 	var game = $(this).attr("data-game");
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
         game + "&api_key=dc6zaTOxFJmzC&limit=10";
 
 	$.ajax({
